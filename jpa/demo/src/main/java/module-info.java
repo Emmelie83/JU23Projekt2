e2cbc72581665ddac307f3eba0b1ad3c853e1b67
Emmelie Johansson <info@emmeliejohansson.com>
2024-01-03 14:22:02 +0100

    Removed demo

--- jpa/demo/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.demo {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires jpa;
-
-    opens org.example.demo to javafx.fxml;
-    exports org.example.demo;
-}
